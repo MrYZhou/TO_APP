48a3c416cb21291627107033399e96022c70f078
22719 <176286175@qq.com>
2020-03-11 19:54:09 +0800

    sec

--- src/App.vue
+++ src/App.vue
@@ -1,23 +1,24 @@
 <template>
-  <div id="app">
-    <img src="./assets/logo.png">
-    <router-view/>
-  </div>
+	<div>
+		<router-link to='/'></router-link>
+		<FooterGuide></FooterGuide>
+	</div>
 </template>
 
 <script>
+import FooterGuide from './components/FooterGuide/FooterGuide.vue'
 export default {
-  name: 'App'
+	data() {
+		return {
+
+		}
+	},
+	components: {
+		FooterGuide
+	}
 }
 </script>
 
-<style>
-#app {
-  font-family: 'Avenir', Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
+<style lang="stylus" ref="stylus">
+
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,32 @@
 import Vue from 'vue'
 import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
-
+import MSite from '../pages/MSite/MSite.vue'
+import Profile from '../pages/Profile/Profile.vue'
+import Order from '../pages/Order/Order.vue'
+import Search from '../pages/Search/Search.vue'
 Vue.use(Router)
 
 export default new Router({
   routes: [
     {
+      path: '/msite',
+      component: MSite
+    },
+    {
+      path: '/profile',
+      component: Profile
+    },
+    {
+      path: '/order',
+      component: Order
+    },
+    {
+      path: '/search',
+      component: Search
+    },
+    {
       path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
+      component: MSite
     }
   ]
 })
